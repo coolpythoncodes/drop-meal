588580fbc5346509c19a7c8b5ab80387bbdf8272
coolpythoncodes <coolpythoncodes@gmail.com>
2021-01-14 23:45:53 +0100

    button option to link to within or outside the webite

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,28 @@
 import styled from 'styled-components';
+import { Link } from 'react-scroll';
 
-const Wrapper = styled.div`
+
+const InternalLink = styled(Link)`
     display: flex;
     justify-content: center;
     align-items: center;
+    cursor: pointer;
+    background: ${props => props.backgroundColor};
+    color: ${props => props.color};
+    width: ${props => props.width}px;
+    height: ${props => props.height}px;
+    margin-right: ${props => props.marginRight}px;
+    ${
+        props => props.border && `border: 1px solid ${props.borderColor};`
+    }
+    border-radius: ${props => props.borderRadius}px;
+
+`
+const Wrapper = styled.a`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
     background: ${props => props.backgroundColor};
     color: ${props => props.color};
     width: ${props => props.width}px;
@@ -33,20 +52,47 @@ const Button = ({
         borderRadius,
         borderColor,
         marginRight,
+        internalLink,
+        href,
+        offset
     }) => {
     return (
-        <Wrapper 
-            color={color}
-            backgroundColor={backgroundColor}
-            width={width}
-            height={height}
-            borderRadius={borderRadius}
-            border={border}
-            borderColor={borderColor}
-            marginRight={marginRight}
-        >
-            <Text>{children}</Text>
-        </Wrapper>
+        <>
+            {
+                internalLink ?        
+                <InternalLink 
+                    color={color}
+                    backgroundColor={backgroundColor}
+                    width={width}
+                    height={height}
+                    borderRadius={borderRadius}
+                    border={border}
+                    borderColor={borderColor}
+                    marginRight={marginRight}
+                    to={href}
+                    spy='true'
+                    activeClass='active'
+                    smooth={true}
+                    duration={100}
+                    offset={offset}
+                >
+                    <Text>{children}</Text>
+                </InternalLink>
+            : 
+                <Wrapper
+                     color={color}
+                     backgroundColor={backgroundColor}
+                     width={width}
+                     height={height}
+                     borderRadius={borderRadius}
+                     border={border}
+                     borderColor={borderColor}
+                     marginRight={marginRight}
+                >
+                    <Text>{children}</Text>
+                </Wrapper>
+            }
+        </>
     );
 }
 
