df61bbdbd290cf6e4a78ff94c9aa4135b3e6ffcc
coolpythoncodes <coolpythoncodes@gmail.com>
2021-01-15 12:11:22 +0100

    stopped using styled-components on Works and Work

--- src/components/Works Section/Works.js
+++ src/components/Works Section/Works.js
@@ -1,29 +1,10 @@
-import styled from 'styled-components'
 import { worksData } from '../../howItWorksData';
 import Work from './Work';
-
-const Wrapper = styled.div`
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    column-gap: 3rem;
-    width: 80%;
-    margin: auto;
-
-    @media screen and (max-width: 767px){
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-    }
-
-    @media screen and (max-width: 300px){
-        width: 90%;
-    }
-`
+import '../../Sass/Works.scss';
 
 const Works = () => {
     return (
-        <Wrapper>
+        <div className='works'>
             {
                 worksData.map(({ 
                     id, 
@@ -38,7 +19,7 @@ const Works = () => {
                         
                         />)
             }
-        </Wrapper>
+        </div>
     );
 }
 
